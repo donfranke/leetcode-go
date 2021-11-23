938761c6ad7b860bb183837f7583db1421670f1e
Don Franke <franke.don@gmail.com>
2021-11-23 08:50:34 -0600

    Cleaned up folders and files

---
+++ problem7/problem7.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world")
+
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+
+}
