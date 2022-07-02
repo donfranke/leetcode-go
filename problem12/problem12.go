bfaf79fdbd3ef62b230f25c19d49cf32c31c7b28
Don Franke <franke.don@gmail.com>
2022-07-02 09:10:29 -0500

    Faster than 14.03%

--- problem12/problem12.go
+++ problem12/problem12.go
@@ -12,35 +12,50 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+// search binary tree
 func searchBST(root *TreeNode, val int) *TreeNode {
-	// 1. search binary tree
-
-	return root
+	if root != nil {
+		if val == root.Val {
+			return root
+		} else if val > root.Val {
+			return searchBST(root.Right, val)
+		} else if val < root.Val {
+			return searchBST(root.Left, val)
+		}
+	}
+	return nil
 }
 
 func main() {
-	fmt.Println("Hello World")
-
 	// build Btree for testing purposes only
-
-	// Input: root = [4,2,7,1,3], val = 2
 	nums := []int{4, 2, 7, 1, 3}
-	// create binary tree
-	var t TreeNode
-	// root node
-	t.Val = nums[0]
 
-	// iterate remaining nums
+	// create binary tree
+	bt := &TreeNode{Val: nums[0]}
 	for i := 1; i < len(nums); i++ {
-		if nums[i] < nums[i-1] {
-			t.Left = &TreeNode{Val: nums[i]}
+		bt.Insert(nums[i])
+	}
+	fmt.Printf("Test: %v\n", bt)
+
+	// search for root matching value
+	bt_root := searchBST(bt, 5)
+	fmt.Printf("Root: %v\n", bt_root)
+}
+
+// Insert node into tree - to build btree for testing purposes only
+// Based on https://www.youtube.com/watch?v=-oYitelECuQ&t=491s
+func (n *TreeNode) Insert(k int) {
+	if k > n.Val {
+		if n.Right == nil {
+			n.Right = &TreeNode{Val: k}
 		} else {
-			t.Right = &TreeNode{Val: nums[i]}
+			n.Right.Insert(k)
+		}
+	} else {
+		if n.Left == nil {
+			n.Left = &TreeNode{Val: k}
+		} else {
+			n.Left.Insert(k)
 		}
 	}
-
-	// Output: [2,1,3]
-
-	// build binary tree
-
 }
